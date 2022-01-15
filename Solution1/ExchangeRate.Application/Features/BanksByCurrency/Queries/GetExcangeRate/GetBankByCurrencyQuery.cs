d215273433e8f65ca24236565df96f6a5022bdd9
Rmishvelidze <80752792+Rmishvelidze@users.noreply.github.com>
2022-01-15 20:50:16 +0400

    BankByCurrency Query

---
+++ Solution1/ExchangeRate.Application/Features/BanksByCurrency/Queries/GetExcangeRate/GetBankByCurrencyQuery.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreHero.Results;
+using AutoMapper;
+using ExchangeRate.Application.Interfaces.Repositories;
+using MediatR;
+
+namespace ExchangeRate.Application.Features.BanksByCurrency.Queries.GetExcangeRate
+{
+    public class GetBankByCurrencyQuery : IRequest<Result<GetBankByCurrencyResponce>>
+    {
+        public string Currency { get; set; }
+        public ICollection<string> Banks { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public class GetBankByCurrencyQueryHandler : IRequestHandler<GetBankByCurrencyQuery, Result<GetBankByCurrencyResponce>>
+        {
+            private readonly IBankByCurrencyRepository _bankByCurrency;
+            private readonly IMapper _mapper;
+
+            public GetBankByCurrencyQueryHandler(IBankByCurrencyRepository bankByCurrency, IMapper mapper)
+            {
+                _bankByCurrency = bankByCurrency;
+                _mapper = mapper;
+            }
+
+            public async Task<Result<GetBankByCurrencyResponce>> Handle(GetBankByCurrencyQuery query, CancellationToken cancellationToken)
+            {
+                var bankByCurrency = await _bankByCurrency.GetBankByCurrenciesAsync
+                    (query.Currency, query.Banks, query.StartDate, query.EndDate);
+                var mappedBankByCurrency = _mapper.Map< GetBankByCurrencyResponce>(bankByCurrency);
+                return Result<GetBankByCurrencyResponce>.Success(mappedBankByCurrency);
+            }
+        }
+    }
+}
