55d3cf460714918333db2be1674bad2e1a9e9f28
Rmishvelidze <80752792+Rmishvelidze@users.noreply.github.com>
2022-01-15 21:00:26 +0400

    Mapping

---
+++ Solution1/ExchangeRate.Application/Mappings/BankByCurrencyProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ExchangeRate.Domain.Entities.Catalog;
+
+namespace ExchangeRate.Application.Mappings
+{
+    internal class BankByCurrencyProfile : Profile
+    {
+        public BankByCurrencyProfile()
+        {
+            CreateMap<BankByCurrencyProfile,ExchangeRateData>().ReverseMap();
+        }
+    }
+}
---
+++ Solution1/ExchangeRate.Application/Mappings/BankProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ExchangeRate.Application.Features.Banks.Queries.GetAll;
+using ExchangeRate.Domain.Entities.Catalog;
+
+namespace ExchangeRate.Application.Mappings
+{
+    internal class BankProfile : Profile
+    {
+        public BankProfile()
+        {
+            CreateMap<GetAllBanksQuery, Bank>().ReverseMap();
+        }
+    }
+}
