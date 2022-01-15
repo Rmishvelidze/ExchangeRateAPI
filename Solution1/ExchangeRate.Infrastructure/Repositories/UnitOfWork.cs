cff493d7ad9ebe3652f23e777b54880e54d24c5b
Rmishvelidze <80752792+Rmishvelidze@users.noreply.github.com>
2022-01-16 00:52:53 +0400

    UnitOfWork

---
+++ Solution1/ExchangeRate.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using ExchangeRate.Application.Interfaces.Repositories;
+using ExchangeRate.Infrastructure.DbContext;
+
+namespace ExchangeRate.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private bool disposed;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<int> Commit(CancellationToken cancellationToken)
+        {
+            return await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public Task Rollback()
+        {
+            //todo
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                    _dbContext.Dispose();
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+    }
+}
